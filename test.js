28db980074c7db730f7ec6609ffcf098deca5085
finalizer_28 <finalizer2869@gmail.com>
2021-11-05 17:50:55 +0100

    stash commit

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ function go() {
 
 function oh() {
     console.log(2 + 2);
+
 }
 
 function inminent_departure() {
@@ -13,5 +14,9 @@ function inminent_departure() {
 }
 
 function stash() {
+    console.log("stas");
+}
+
+function stash2() {
     console.log("stas");
 }
