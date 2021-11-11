dc5cb8ad4b3dcb70e2e6fae4d3c7dfdb47d64e4a
finalizer_28 <finalizer2869@gmail.com>
2021-11-11 17:49:16 +0100

    uasw

---
+++ Module 4 (1.2)/karpeta/karpeta_func/Module 3 (2)/test.js
@@ -0,0 +1,53 @@
+console.log("test");
+
+function go() {
+    console.log("coas");
+}
+
+function oh() {
+    console.log(2 + 2);
+
+}
+
+function inminent_departure() {
+    console.log("cssp");
+}
+
+
+
+function stash() {
+    console.log("stas");
+}
+
+function stash2() {
+    console.log("stas");
+}
+
+function merge() {
+    console.log("merg");
+
+}
+
+function redirect() {
+    window.URL = "http://127.0.0.1/login_usuario";
+}
+
+function se() {
+    var x = document.getElementById("togs");
+    if (x.type === "password") {
+        x.type = "text";
+        console.log("pass")
+    } else {
+        x.type = "password";
+        console.log("text")
+    }
+}
+
+function megre2() {
+    console.log("msef");
+
+}
+
+function mainbranch() {
+    console.log("mainsbr");
+}
